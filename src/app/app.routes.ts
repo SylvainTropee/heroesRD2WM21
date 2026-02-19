630b6bd2d16f1c97076ec4d4f1be55d3c15e49bd
stropee <stropee@campus-eni.fr>
2026-02-19 16:57:19 +0100

    heores navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {AllHeroes} from './components/all-heroes/all-heroes';
+import {HeroDetail} from './components/hero-detail/hero-detail';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path : "", component : AllHeroes},
+  {path : "hero/:id", component : HeroDetail},
+  {path : "**", component : AllHeroes}
+];
